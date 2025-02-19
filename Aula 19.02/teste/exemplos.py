c6da1fad80293019fc5c1973b31cf030e7201022
~Bella <maria.bella0619@gmail.com>
2025-02-19 11:39:34 -0300

    Update exemplos.py

---
+++ Aula 19.02/teste/exemplos.py
@@ -0,0 +1,30 @@
+# declaração de variavel
+variavel = 'bell'
+
+# declaração de variavel
+def mostrar ():
+    print ("nao")
+
+    if (mostrar < 1):
+          print ('ok')
+    
+    elif (mostrar > 5):
+         print ('maior que 5')
+
+    else:
+         print ('not ok') 
+
+    for i in range(10):
+         print (i)
+
+
+lista_de_frutas = ['laranja', 'banana', 'ameixa', 1, True]
+
+print (lista_de_frutas.append('laranja'))
+
+for fruta in range (len(lista_de_frutas)):
+     
+     
+     print(fruta)
+
+
