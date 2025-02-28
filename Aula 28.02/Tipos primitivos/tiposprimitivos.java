e6575da5180283b1fc292195916a6c86c3d57d3e
~Bella <maria.bella0619@gmail.com>
2025-02-28 11:33:36 -0300

    Add files via upload

---
+++ Aula 28.02/Tipos primitivos/tiposprimitivos.java
@@ -0,0 +1,19 @@
+public class tiposprimitivos {
+
+    public static void main (String args[]){
+        boolean flag = true;
+        char ch = 'A';
+        byte b = 12;
+        short s =  24;
+        int i = 123;
+        long l = 890L;
+
+        System.out.println("Variavel booleana = " + flag);
+        System.out.println("Variavel char = " + ch);
+        System.out.println("Variavel byte = " + b);
+        System.out.println("Variavel short = " + s);
+        System.out.println("Variavel int = " + i);
+        System.out.println("Variavel long = " + l);
+        System.out.println("Variavel booleana = " + flag);
+    }
+}
